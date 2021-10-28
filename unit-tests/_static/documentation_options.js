3c184062d0fd7b33a8ab8fe57bbbde5933fc030d
john-wigg <john-wigg@users.noreply.github.com>
2021-10-28 20:05:43 +0000

    deploy: 92196f35daa9d11db3ab3271180f9989973cd9fd

--- unit-tests/_static/documentation_options.js
+++ unit-tests/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.1-7-g1a55b90',
+    VERSION: '1.1-8-g92196f3',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
