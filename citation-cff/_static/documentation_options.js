6b056a421aef84601472842f5327d2e22a10b712
john-wigg <john-wigg@users.noreply.github.com>
2021-11-01 13:51:23 +0000

    deploy: b9031fae397a44b34e1f8bf3100a34180916968c

--- citation-cff/_static/documentation_options.js
+++ citation-cff/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.1-10-g3e3151d',
+    VERSION: '1.1-11-gb9031fa',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
